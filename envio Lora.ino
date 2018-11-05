da1758280218757f81b6ec919d5f2f811da79950
rodrigoalj1 <rodrigoalj1@gmail.com>
2018-11-05 20:05:29 -0300

    Lora envío y recepción

---
+++ envio Lora.ino
@@ -0,0 +1,33 @@
+#define SCK     15
+  #define MISO    14
+  #define MOSI    16
+  #define SS      8
+  #define RST     4
+  #define DI0     7
+  #define BAND    868E6  // 915E6
+  #define PABOOST true 
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("LoRa Sender");
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(BAND,PABOOST)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+  counter++;
+  delay(5000);
+}
